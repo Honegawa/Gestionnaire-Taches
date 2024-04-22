f6b700e874f12bd3666e11318470ef1c2b019474
Axel <axel.ein@gmail.com>
2024-04-22 15:27:04 +0200

    added the user sign-up form and logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import {Routes, Route} from 'react-router-dom'
 import Template from './components/Template/Template.jsx'
+import PageSignUp from './pages/PageSignUp.tsx'
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Template/>}> 
+      <Route path="/" element={<Template/>}>
+      <Route path='/sign-up' element={<PageSignUp />} />
         {/* <Route path='/home' element={<PageHome />} />
-        <Route path='/sign-up' element={<PageSignUp />} />
         <Route path='/dashboard' element={<PageDashboard />} /> */}
       </Route>
   </Routes>
