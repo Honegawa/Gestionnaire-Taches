6e3adc93c796318958ccb3a4cdc1dff38fba9a09
Steven TAING <taing.steven@outlook.fr>
2024-04-23 22:28:58 +0200

    Added styles

--- src/components/Template/Template.tsx
+++ src/components/Template/Template.tsx
@@ -1,15 +1,18 @@
+import "./Template.css";
 import { Outlet } from "react-router-dom";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 
 const Template = () => {
-    return (
-        <>
-            <Header />
-            <Outlet />
-            <Footer />
-        </>
-    );
+  return (
+    <div className="layout">
+      <Header />
+      <section>
+        <Outlet />
+      </section>
+      <Footer />
+    </div>
+  );
 };
 
 export default Template;
--- src/pages/PageSignUp.tsx
+++ src/pages/PageSignUp.tsx
@@ -1,49 +1,72 @@
-import { useState } from 'react';
-import axios from 'axios';
-import {URL} from '../utils/constants/url.ts';
-import {User} from '../interfaces/user.ts';
+import "./PageSignUp.css";
+import { useState } from "react";
+import axios from "axios";
+import { URL } from "../utils/constants/url.ts";
+import { User } from "../interfaces/user.ts";
+import { Link, useNavigate } from "react-router-dom";
 
 function PageSignUp() {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+  const navigate = useNavigate();
 
-    const [user, setUser] = useState(
-        {
-            email: '',
-            password: '',
-        }
-    )
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = event.target;
+    setUser((user: User) => ({ ...user, [name]: value }));
+  };
 
-    const handleChange = (event:React.ChangeEvent<HTMLInputElement>) => { // Si plusieurs type mettre un |
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
 
-        const { name, value } = event.target;
-        setUser( (user: User) => ( { ...user, [name]: value } ) );
-    };
+    try {
+      const response = await axios.post(URL.USER_SIGN_UP, user);
+      const { status } = response;
 
-    const handleSubmit = async (event:React.FormEvent<HTMLFormElement>) => { // React.FormEvent
-        event.preventDefault();
-
-        try {
-            const response = await axios.post(URL.USER_SIGN_UP, user)
-            console.log(response);
-        } catch (error) {
-            console.log(error);
-        }
+      if (status === 201) {
+        navigate("/");
+      }
+      console.log(response);
+    } catch (error) {
+      console.log(error);
     }
+  };
 
-    return (
-        <>
-            <h1>Page Ajout Utilisateur</h1>
+  return (
+    <>
+      <h1>Inscription</h1>
 
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="email">Nom :</label>
-                <input name="email" type="text" onChange={handleChange} />
+      <form onSubmit={handleSubmit}>
+        <div className="signUpForm">
+          <div>
+            <label htmlFor="email">Email</label>
+            <input
+              id="email"
+              name="email"
+              type="text"
+              onChange={handleChange}
+            />
+          </div>
 
-                <label htmlFor="password">Mot de passe :</label>
-                <input name="password" type="password" onChange={handleChange} />
+          <div>
+            <label htmlFor="password">Mot de passe</label>
+            <input
+              id="password"
+              name="password"
+              type="password"
+              onChange={handleChange}
+            />
+          </div>
+          <button>S'Inscrire</button>
+        </div>
+      </form>
 
-                <button>S'Inscrire</button>
-            </form>
-        </>
-    );
+      <Link to="/">
+        <button>Retour à l'accueil</button>
+      </Link>
+    </>
+  );
 }
 
-export default PageSignUp
+export default PageSignUp;
