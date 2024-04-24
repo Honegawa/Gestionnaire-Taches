1147088047c21b201213c7c488bdaa3f6cc04040
Steven TAING <taing.steven@outlook.fr>
2024-04-24 19:03:05 +0200

    Implemented Redux Task + Fixed category select bug

--- src/redux/reducers/task.ts
+++ src/redux/reducers/task.ts
@@ -1,5 +1,5 @@
 import { PayloadAction, createSlice } from "@reduxjs/toolkit";
-import { Task } from "../../interfaces/task";
+import { DeletedTask, Task, UpdatedTask } from "../../interfaces/task";
 
 type initialTask = {
   data: Task[];
@@ -21,10 +21,7 @@ export const Tasks = createSlice({
     FETCH_START: (store: initialTask) => {
       store.loading = true;
     },
-    FETCH_SUCCESS: (
-      store: initialTask,
-      actions: PayloadAction<Task[]>
-    ) => {
+    FETCH_SUCCESS: (store: initialTask, actions: PayloadAction<Task[]>) => {
       store.loading = false;
       store.data = actions.payload;
     },
@@ -32,30 +29,76 @@ export const Tasks = createSlice({
       store.loading = false;
       store.error = true;
     },
-    // POST_START: (store: initialTask) => {
-    //   store.loading = true;
-    // },
-    // POST_SUCCESS: (
-    //   store: initialTask,
-    //   actions: PayloadAction<Task>
-    // ) => {
-    //   store.loading = false;
-    //   store.data.push(actions.payload);
-    // },
-    // POST_FAILURE: (store: initialTask) => {
-    //   store.loading = false;
-    //   store.error = true;
-    // },
+    POST_START: (store: initialTask) => {
+      store.loading = true;
+    },
+    POST_SUCCESS: (store: initialTask, actions: PayloadAction<Task>) => {
+      store.loading = false;
+      store.data.push(actions.payload);
+    },
+    POST_FAILURE: (store: initialTask) => {
+      store.loading = false;
+      store.error = true;
+    },
+    UPDATE_START: (store: initialTask) => {
+      store.loading = true;
+    },
+    UPDATE_SUCCESS: (
+      store: initialTask,
+      actions: PayloadAction<UpdatedTask>
+    ) => {
+      const newTask = actions.payload.update;
+      const tasks = actions.payload.data;
+      const newTasks: Task[] = [];
+
+      tasks.map((task: Task) => {
+        if (task.id === newTask.id) {
+          console.log(newTask.category);
+          newTasks.push(newTask);
+        } else {
+          newTasks.push(task);
+        }
+      });
+
+      store.loading = false;
+      store.data = newTasks;
+    },
+    UPDATE_FAILURE: (store: initialTask) => {
+      store.loading = false;
+      store.error = true;
+    },
+    DELETE_START: (store: initialTask) => {
+      store.loading = true;
+    },
+    DELETE_SUCCESS: (
+      store: initialTask,
+      actions: PayloadAction<DeletedTask>
+    ) => {
+      store.loading = false;
+      store.data = actions.payload.data.filter(
+        (task: Task) => task.id !== actions.payload.id
+      );
+    },
+    DELETE_FAILURE: (store: initialTask) => {
+      store.loading = false;
+      store.error = true;
+    },
   },
 });
 
-export const { 
-    FETCH_START, 
-    FETCH_SUCCESS, 
-    FETCH_FAILURE, 
-    // POST_START,
-    // POST_SUCCESS, 
-    // POST_FAILURE 
+export const {
+  FETCH_START,
+  FETCH_SUCCESS,
+  FETCH_FAILURE,
+  POST_START,
+  POST_SUCCESS,
+  POST_FAILURE,
+  UPDATE_START,
+  UPDATE_SUCCESS,
+  UPDATE_FAILURE,
+  DELETE_START,
+  DELETE_SUCCESS,
+  DELETE_FAILURE,
 } = Tasks.actions;
 
 export default Tasks.reducer;
--- src/service/selector/task.selector.ts
+++ src/service/selector/task.selector.ts
@@ -2,12 +2,4 @@ import { RootState } from "../../interfaces/task"
 
 export const allTasks = (state: RootState) => {
   return state.tasks.data
-} 
-
-
-
-
-
-
-
-
+}
