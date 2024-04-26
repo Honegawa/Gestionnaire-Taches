688c5a79532bfcaf1545e8cbcc3bb4e726600b4e
Steven TAING <taing.steven@outlook.fr>
2024-04-26 10:55:02 +0200

    Fixed warning of route /

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,29 @@
-import {Routes, Route} from 'react-router-dom';
-import PageSignUp from './pages/PageSignUp.tsx';
-import PageHome from './pages/PageHome.tsx';
-import PageDashboard from './pages/PageDashboard.tsx';
-import TemplateLogged from './components/Template/TemplateLogged.tsx';
-import TemplateNotLogged from './components/Template/TemplateNotLogged.tsx';
+import { Routes, Route, Navigate } from "react-router-dom";
+import PageSignUp from "./pages/PageSignUp.tsx";
+import PageHome from "./pages/PageHome.tsx";
+import PageDashboard from "./pages/PageDashboard.tsx";
+import TemplateLogged from "./components/Template/TemplateLogged.tsx";
+import TemplateNotLogged from "./components/Template/TemplateNotLogged.tsx";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path='/dashboard' element={<TemplateLogged/>}>
-          <Route index element={<PageDashboard />} />
+        <Route path="/">
+          <Route path="/dashboard" element={<TemplateLogged />}>
+            <Route index element={<PageDashboard />} />
+          </Route>
+
+          <Route element={<TemplateNotLogged />}>
+            <Route index element={<PageHome />} />
+            <Route path="sign-up" element={<PageSignUp />} />
+          </Route>
         </Route>
-      </Routes>
-      <Routes>
-        <Route path='/' element={<TemplateNotLogged/>}>
-          <Route index element={<PageHome />} />
-          <Route path='sign-up' element={<PageSignUp />} />
-        </Route>
+
+        <Route path="*" element={<Navigate to="/"/>}/>
       </Routes>
     </>
-
-  )
+  );
 }
 
 export default App;
