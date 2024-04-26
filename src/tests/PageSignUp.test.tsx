0379b0635de6048d976748d7e76f6193d8d24477
Axel <axel.ein@gmail.com>
2024-04-26 17:48:54 +0200

    fixed sign-up confirmation message and unit test

--- src/tests/PageSignUp.test.tsx
+++ src/tests/PageSignUp.test.tsx
@@ -15,7 +15,6 @@ describe("Inscription", () => {
       </BrowserRouter>
     );
 
-    // SIMULE UNE 201 si l'api AXIOS POST est appellé
     const mock = jest.spyOn(Axios, "post");
     mock.mockImplementation(() => Promise.resolve(
       {
@@ -28,25 +27,15 @@ describe("Inscription", () => {
 
     const emailInput = getByLabelText("Email") as HTMLInputElement;
     fireEvent.change(emailInput, { target: { value: "test@test.mail" } });
-    const mail = emailInput.value;
 
     const passwordInput = getByLabelText("Mot de passe") as HTMLInputElement;
     fireEvent.change(passwordInput, { target: { value: "test" } });
-    const mdp = passwordInput.value;
 
     const button = getByText("S'Inscrire");
     fireEvent.click(button);
 
-    const mockedUser:any = {
-      email: mail,
-      password: mdp,
-    }
-
-    await Axios.post('http://localhost:5678/api/users/signup', mockedUser);
-
-    // waitFor permet d'attendre que
     await waitFor(
-      () => expect(getByText('Féliciation, vous êtes bien inscrit !')).toBeInTheDocument()
+      () => expect(getByText('Félicitation, vous êtes bien inscrit !')).toBeInTheDocument()
     );
 
   });
